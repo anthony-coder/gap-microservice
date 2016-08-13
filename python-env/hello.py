6c15e91fdec197e0c18106c57caa69338800e59a
RichardWLaub <RichardWLaub@gmail.com>
2016-08-12 17:42:10 -0700

    comments

--- python-env/hello.py
+++ python-env/hello.py
@@ -4,15 +4,26 @@ from flask.ext.script import Manager
 
 app = Flask(__name__)
 
+# manager allows for threaded option
 manager = Manager(app)
 
 @app.route('/<input>')
 def process(input):
+
+    # defines where python looks for the docker server
     cli = Client(base_url='unix://var/run/docker.sock')
+
+    # launching a new gap container and giving it the environment varible <input>
     container = cli.create_container(image='rwlaub/gap', environment=["ARRAY=" + input])
     cli.start(container['Id'])
+
+    # waiting for gap to finish calculation
     cli.wait(container['Id'])
+
+    # reading gap output from logs
     output = cli.logs(container['Id'], tail=1)
+
+    # cleanup and output
     cli.remove_container(container['Id'])
     return output
 
